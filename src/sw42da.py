e526e0dc48bc6d5bc04781e8391fdb47ea3ed4be
chillywasher <cwhalley72@gmail.com>
2025-12-16 15:25:08 +0000

    hard code connection variables

--- src/sw42da.py
+++ src/sw42da.py
@@ -3,7 +3,7 @@ import os
 import serial
 from dotenv import load_dotenv
 
-load_dotenv()
+# load_dotenv()
 
 # if os.path.isfile("/data/options.json"):
 #     json_path = "/data/options.json"
@@ -17,35 +17,31 @@ load_dotenv()
 #
 # print(config)
 
-HOST_IP = os.getenv("HOST_IP")
-HOST_PORT = os.getenv("HOST_PORT")
-HOST_BAUDRATE = os.getenv("HOST_BAUDRATE")
+# HOST_IP = os.getenv("HOST_IP")
+# HOST_PORT = os.getenv("HOST_PORT")
+# HOST_BAUDRATE = os.getenv("HOST_BAUDRATE")
+#
+# if not HOST_IP:
+#     raise Exception("Could not load HOST_IP variable")
 
-if not HOST_IP:
-    raise Exception("Could not load HOST_IP variable")
-
-_url = f"socket://{HOST_IP}:{HOST_PORT}"
-print(_url)
+# _url = f"socket://sw42da.local:8000"
+# print(_url)
 
 
 class Sw42da:
 
     def __init__(self):
-        self._url = f"socket://{HOST_IP}:{HOST_PORT}"
+        self._url = f"socket://sw42da.local:8000"
 
 
     async def send_command(self, c: str):
-
-        if not HOST_IP or not HOST_PORT or not HOST_BAUDRATE:
-            print("Waiting for config options")
-            return None
 
         if not c.endswith("\n"):
             c = c + "\n"
 
         b = bytes(c, "UTF-8")
 
-        ser = serial.serial_for_url(url=self._url, stopbits=1, bytesize=8, baudrate=HOST_BAUDRATE, parity="N", timeout=0.5)
+        ser = serial.serial_for_url(url=self._url, stopbits=1, bytesize=8, baudrate=57600, parity="N", timeout=0.5)
         ser.write(b)
 
         response = []
